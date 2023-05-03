f495fa003567155528b6345fa4ea5faf0d57e414
unknown <r0840938@student.thomasmore.be>
2023-05-03 15:16:44 +0200

    scripts

---
+++ my scripts/export_data_telegram_chat.py
@@ -0,0 +1,71 @@
+from telethon.sync import TelegramClient
+import customtkinter
+import tkinter
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.tl.types import MessageMediaPhoto
+from tqdm import tqdm
+import csv
+import os
+
+api_id = 29897393       # Your API ID
+api_hash = 'dc658af18895c141e58532591ccbb239'  # Your API HASH
+title_Download_Folder = 'DOWNLOADS'
+chat_name = str(input("chat name: "))
+chat_name = '🔥🔞 HOOFDDOEKJES 18+ 🔥🍑'
+print("images(1)")
+print("videos(2)")
+print("files(3)")
+choice = int(input("What do you want to export: "))
+
+
+
+def download_media(group, cl, name, offset_id):
+    messages = cl.get_messages(group, limit=3000, offset_id=offset_id)
+
+    for message in tqdm(messages):
+        if choice == 1 and isinstance(message.media, MessageMediaPhoto):
+            file_path = message.download_media('./' + name + '/')
+            if file_path:
+                file_name = os.path.basename(file_path)
+                with open('images.csv', 'a', newline='') as f:
+                    writer = csv.writer(f, delimiter=';')
+                    writer.writerow([message.id, file_name])
+        elif choice == 2 and message.video:
+            file_path = message.download_media('./' + name + '/')
+            if file_path:
+                file_name = os.path.basename(file_path)
+                with open('videos.csv', 'a', newline='') as f:
+                    writer = csv.writer(f, delimiter=';')
+                    writer.writerow([message.id, file_name])
+        elif choice == 3 and message.file:
+            file_path = message.download_media('./' + name + '/')
+            if file_path:
+                file_name = os.path.basename(file_path)
+                with open('files.csv', 'a', newline='') as f:
+                    writer = csv.writer(f, delimiter=';')
+                    writer.writerow([message.id, file_name])
+
+    return messages[-1].id if messages else offset_id
+
+def download_all_media(chat, client, title_Download_Folder):
+    offset_id = 0
+    while True:
+        try:
+            offset_id = download_media(chat, client, title_Download_Folder, offset_id)
+        except ValueError:
+            print("All messages downloaded.")
+            break
+    print("DONE")
+
+with TelegramClient('name', api_id, api_hash) as client:
+    result = client(GetDialogsRequest(
+        offset_date=None,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=500,
+        hash=0,
+    ))
+
+    download_all_media(chat_name, client, title_Download_Folder)
---
+++ my scripts/export_userID_chat.py
@@ -0,0 +1,63 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest, GetHistoryRequest
+from telethon.tl.types import InputPeerEmpty
+from tqdm import tqdm
+import csv
+
+api_id = 29897393       # Your API ID
+api_hash = 'dc658af18895c141e58532591ccbb239'  # Your API HASH
+chat_name = 'TEST'
+user_id = 5120940718
+
+def export_messages(client, chat, user_id):
+    offset_id = 0
+    limit = 100
+    all_messages = []
+
+    while True:
+        messages = client(GetHistoryRequest(
+            peer=chat,
+            offset_id=offset_id,
+            offset_date=None,
+            add_offset=0,
+            limit=limit,
+            max_id=0,
+            min_id=0,
+            hash=0
+        ))
+
+        if not messages.messages:
+            break
+
+        for message in messages.messages:
+            if message.from_id.user_id == user_id:
+                all_messages.append((message.id, message.date, message.message))
+
+        offset_id = messages.messages[-1].id
+
+    with open('BERICHTEN.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Message ID', 'Sent Timestamp', 'Message Text'])
+        for message in all_messages:
+            writer.writerow([message[0], message[1], message[2]])
+
+    print(f"{len(all_messages)} messages exported to BERICHTEN.csv")
+
+with TelegramClient('name', api_id, api_hash) as client:
+    # Get the dialogs
+    result = client(GetDialogsRequest(
+        offset_date=None,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=500,
+        hash=0,
+    ))
+
+    # Find the chat with the specified name
+    for chat in result.chats:
+        if chat.title == chat_name:
+            target_chat = chat
+            break
+
+    # Export all messages sent by the specified user in the target chat
+    export_messages(client, target_chat, user_id)
